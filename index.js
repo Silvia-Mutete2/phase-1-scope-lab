9445664e9e6e2608eb06ce0f270f0a7e50d877ba
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-06-07 23:10:36 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName ='bob';
+const leastFavoriteCustomer = 'mercy'
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase();
+}
+upperCaseCustomerName();
+
+function setBestCustomer(){
+    bestCustomer ='not bob';
+}
+function overwriteBestCustomer(favorite){
+    bestCustomer = favorite;
+
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'jane'
+
+}
